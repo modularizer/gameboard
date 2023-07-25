edd3dde884198217ae2ced9d4ae0f7a2bcdc2bdf
modularizer <modularizer@gmail.com>
2023-07-24 22:04:30 -0700

    orbit

--- js/scene.js
+++ js/scene.js
@@ -1,6 +1,5 @@
-// assuming OrbitControls is available in the global scope or has been imported
 class CustomScene extends THREE.Scene {
-    constructor() {
+    constructor(item) {
         super();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.z = 5;
@@ -10,6 +9,25 @@ class CustomScene extends THREE.Scene {
 
         // OrbitControls for panning, zooming, and rotating
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+         this.isDragging = false;
+        this.controls.addEventListener('start', () => {
+            this.shouldRotate = false;
+            this.isDragging = true;
+        });
+        this.controls.addEventListener('end', () => {
+            this.shouldRotate = true;
+            this.isDragging = false;
+        });
+
+
+        this.shouldRotate = true; // Whether or not the item should rotate
+
+        // Event listener for spacebar
+        window.addEventListener('keydown', (event) => {
+            if (event.code === 'Space' && !this.isDragging) {
+                this.shouldRotate = !this.shouldRotate; // Toggle rotation
+            }
+        });
 
         this.display = this.display.bind(this);
         this.animate = this.animate.bind(this);
@@ -18,8 +36,9 @@ class CustomScene extends THREE.Scene {
         parentElement.appendChild(this.renderer.domElement);
     }
     animate(item) {
+        this.item = item;
         requestAnimationFrame(this.animate.bind(this, item));
-        item.rotate(0.02, 0.01);
+        if (this.shouldRotate) item.rotate(0.02, 0.01);
         this.controls.update();  // update OrbitControls
         this.renderer.render(this, this.camera);
     }
