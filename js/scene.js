fa50386604d2426003310387cbba2a86e7c7c1fe
modularizer <modularizer@gmail.com>
2023-07-24 22:11:52 -0700

    orbit

--- js/scene.js
+++ js/scene.js
@@ -29,8 +29,61 @@ class CustomScene extends THREE.Scene {
             }
         });
 
+        // Event listener for arrow keys
+        window.addEventListener('keydown', (e) => {
+            const speed = 0.1;  // adjust this value to control speed
+            switch (e.code) {
+                case 'ArrowUp':
+                    if (e.ctrlKey) {
+                        this.item.rotateX(speed);
+                    } else {
+                        this.camera.position.y += speed;
+                    }
+                    e.preventDefault();
+                    e.stopPropagation();
+                    break;
+                case 'ArrowDown':
+                    if (e.ctrlKey) {
+                        this.item.rotateX(-speed);
+                    } else {
+                        this.camera.position.y -= speed;
+                    }
+                    e.preventDefault();
+                    e.stopPropagation();
+                    break;
+                case 'ArrowLeft':
+                    if (e.ctrlKey) {
+                        this.item.rotateY(speed);
+                    } else {
+                        this.camera.position.x -= speed;
+                    }
+                    e.preventDefault();
+                    e.stopPropagation();
+                    break;
+                case 'ArrowRight':
+                    if (e.ctrlKey) {
+                        this.item.rotateY(-speed);
+                    } else {
+                        this.item.position.x += speed;
+                    }
+                    e.preventDefault();
+                    e.stopPropagation();
+                    break;
+            }
+        });
+
         this.display = this.display.bind(this);
         this.animate = this.animate.bind(this);
+
+        // Add axes helper
+        const axesHelper = new THREE.AxesHelper(5);
+        this.add(axesHelper);
+
+        // Add grid helper
+        const size = 10;
+        const divisions = 10;
+        const gridHelper = new THREE.GridHelper(size, divisions);
+        this.add(gridHelper);
     }
     display(parentElement) {
         parentElement.appendChild(this.renderer.domElement);
