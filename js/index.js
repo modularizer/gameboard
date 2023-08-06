ee55df5dde6faf5d6be68ac3899299cac061901a
modularizer <modularizer@gmail.com>
2023-08-06 14:13:26 -0700

    paths

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-export { SimpleSnapController, SnapController, SnapNodes, SnapNode } from '../../js/snapControl.js';
-export { BaseModel } from '../../js/components/base_model.js';
-export { load3DModel } from '../../js/components/3d_model.js';
-export { loadCube } from '../../js/components/cube.js';
-export { loadText } from '../../js/components/text.js';
-export { Model, placeModels } from '../../js/components/model.js';
-export { CustomScene } from '../../js/scene.js';
-export { MoveableItem } from '../../js/moveable.js';
+export { SimpleSnapController, SnapController, SnapNodes, SnapNode } from './snapControl.js';
+export { BaseModel } from './components/base_model.js';
+export { load3DModel } from './components/3d_model.js';
+export { loadCube } from './components/cube.js';
+export { loadText } from './components/text.js';
+export { Model, placeModels } from './components/model.js';
+export { CustomScene } from './scene.js';
+export { MoveableItem } from './moveable.js';
