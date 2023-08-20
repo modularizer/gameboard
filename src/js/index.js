beb6525006af46fea38007ff6ee922155447aa6f
modularizer <modularizer@gmail.com>
2023-08-20 12:51:04 -0700

    build

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import { GameBoard } from "./gameboard.js?v20230820T15:08:50.016Z";
-import { ChatBox } from "./utils/chat.js?v20230820T15:08:50.016Z";
+import { GameBoard } from "./gameboard.js?v20230820T19:50:27.032Z";
+import { ChatBox } from "./utils/chat.js?v20230820T19:50:27.032Z";
 
 customElements.define('chat-box', ChatBox);
 customElements.define('game-board', GameBoard);
