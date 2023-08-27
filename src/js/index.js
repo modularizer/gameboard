d5a408a3cc27b9333862f6c8ba8ed60c0e06b1b7
modularizer <modularizer@gmail.com>
2023-08-27 11:02:23 -0700

    blanks

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-window.version = 'v20230827T17:59:37.288Z';
+window.version = 'v20230827T18:02:10.732Z';
 
-import { GameBoard } from "./gameboard.js?v20230827T17:59:37.288Z";
-import { ChatBox } from "./utils/chat.js?v20230827T17:59:37.288Z";
+import { GameBoard } from "./gameboard.js?v20230827T18:02:10.732Z";
+import { ChatBox } from "./utils/chat.js?v20230827T18:02:10.732Z";
 
 customElements.define('chat-box', ChatBox);
 customElements.define('game-board', GameBoard);
