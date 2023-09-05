fda31f94da3a353be804ccf181905af7c9b2d4e8
modularizer <modularizer@gmail.com>
2023-09-04 17:13:54 -0700

    add blank

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-window.version = 'v20230904T22:42:52.629Z';
+window.version = 'v20230905T00:13:44.114Z';
 
-import { GameBoard } from "./gameboard.js?v20230904T22:42:52.629Z";
-import { ChatBox } from "./utils/chat.js?v20230904T22:42:52.629Z";
-import { ScoreCard } from "./utils/score-card.js?v20230904T22:42:52.629Z";
+import { GameBoard } from "./gameboard.js?v20230905T00:13:44.114Z";
+import { ChatBox } from "./utils/chat.js?v20230905T00:13:44.114Z";
+import { ScoreCard } from "./utils/score-card.js?v20230905T00:13:44.114Z";
 
 customElements.define('score-card', ScoreCard);
 customElements.define('chat-box', ChatBox);
