430ceb09024b7dbffc97ffcf148616f731ec61c6
modularizer <modularizer@gmail.com>
2024-05-19 21:29:50 -0700

    mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-window.version = 'v20240520T04:13:25.013Z';
+window.version = 'v20240520T04:29:43.242Z';
 
-import { GameBoard } from "./gameboard.js?v20240520T04:13:25.013Z";
-import { ScoreCard } from "./utils/score-card.js?v20240520T04:13:25.013Z";
+import { GameBoard } from "./gameboard.js?v20240520T04:29:43.242Z";
+import { ScoreCard } from "./utils/score-card.js?v20240520T04:29:43.242Z";
 
 customElements.define('score-card', ScoreCard);
 customElements.define('game-board', GameBoard);
