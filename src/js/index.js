239a639af3c8a2b35f7f78d962600fb515ada90b
modularizer <modularizer@gmail.com>
2024-05-25 12:48:54 -0700

    fix topic

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-window.version = 'v20240525T19:32:31.579Z';
+window.version = 'v20240525T19:48:39.470Z';
 
-import { GameBoard } from "./gameboard.js?v20240525T19:32:31.579Z";
-import { ScoreCard } from "./utils/score-card.js?v20240525T19:32:31.579Z";
+import { GameBoard } from "./gameboard.js?v20240525T19:48:39.470Z";
+import { ScoreCard } from "./utils/score-card.js?v20240525T19:48:39.470Z";
 
 customElements.define('score-card', ScoreCard);
 customElements.define('game-board', GameBoard);
